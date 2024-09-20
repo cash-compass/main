97012a02bc4d1cb91b7b7496dc90355ad55e9c5b
Ciddous <123509482+Ciddous@users.noreply.github.com>
2024-09-20 11:00:08 -0500

    Update functions.js
    Added some basic functions as placeholders for reference

--- functions.js
+++ functions.js
@@ -1,3 +1,28 @@
 function changeColor() {
     document.getElementById("my-div").style.backgroundColor = "#FDF305";
 }
+
+// going to be used for creating the user input for income
+function incomeButton() {
+
+}
+
+// going to be used for creating the user input for expenses 
+function expendituresButton() {
+
+}
+
+// going to be used for saving the user data
+function saveButton() {
+
+}
+
+// used for taking all the data and calculate their budget
+function budgetCalc() {
+
+}
+
+// used for taking all the data and storing into the user profile
+function dataAnaylzer() {
+
+}
