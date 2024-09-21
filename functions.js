16d62959c11c4ce05f4da0a0813d24d6f2c65e7c
bentirado <benjam.tirado@gmail.com>
2024-09-21 14:03:14 -0500

    Merge branch 'main' of https://github.com/cash-compass/main

--- functions.js
+++ functions.js
@@ -1,7 +1,30 @@
 function changeColor() {
     document.getElementById("my-div").style.backgroundColor = "#FDF305";
-}
 
+}
+//This is the user object constructor.
+function user(first_name, last_name, weekly_income, weekly_expense, current_income, current_expense) {
+    this.first_name = '';
+    this.last_name = '';
+    //These two are the projected values for each week.
+    this.weekly_income = 0;
+    this.weekly_expense = 0;
+    //These two are the real world values for each week.
+    this.current_income = 0;
+    this.current_expense = 0;
+}
+//Here is how to create a new object and edit the variables.
+//In the future we will need to have a database of already made user information.
+//For now we will have to hard code it, so lets user this test_user to figure out the different functions and algorithms. :)
+test_user = new user();
+test_user.first_name = "Richard";
+test_user.last_name = "Johnson";
+
+//Function to get username
+function getName(user) {
+    console.log(user.first_name + " " + user.last_name)
+
+}
 // going to be used for creating the user input for income
 function incomeButton() {
 
@@ -26,3 +49,10 @@ function budgetCalc() {
 function dataAnaylzer() {
 
 }
+
+
+//This function updates the user's name when the page loads.
+//This is how you have to wrtie anything that you want to happen right away.
+window.onload = function() {
+   document.getElementById('username_html').innerHTML = test_user.first_name + " " + test_user.last_name;
+};
