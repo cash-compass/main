3b3fd348edad845f8cc2f19217b3751c73ed0e63
bentirado <benjam.tirado@gmail.com>
2024-10-28 18:00:17 -0500

    Added Several Functions to testjava.js
    Made a whole lot of changes to the testjava file. Everything is done on my end.

--- testjava.js
+++ testjava.js
@@ -8,6 +8,7 @@ function user(first_name, last_name, weekly_income, weekly_expense, current_inco
     //These two are the real world values for each week.
     this.current_income = 0;
     this.current_expense = 0;
+    this.surplus = 0;
 }
 //Here is how to create a new object and edit the variables.
 //In the future we will need to have a database of already made user information.
@@ -29,48 +30,53 @@ function getName(user) {
 
 }
 // Function used for the income button
-function incomeButton(num, type, user, date, week) {
+function incomeButton(num, type, user) {
+        var tempDate = checkDate();
+        const date = tempDate.split('/');
         if (num < 0) {
           console.log("'num' parameter is not valid!");
           return 1;
         }
-        else if((date < 0) || (date > 7)) {
+        else if((date[0] < 0) || (date[0] > 7)) {
           console.log("'date' parameter is not valid!");
           return 2;
         }
-        else if (week < 0) {
+        else if (date[1] < 0) {
           console.log("'week' parameter is not valid!");
           return 3;
         }
         else {
         income.push({
           //Converts the string input into an interger.
-          value: num,
+          income: num,
           cat: type,
-          day: date,
-          week: week,
+          day: date[0],
+          week: date[1],
         })
         user.current_income+=num;
         len = income.length;
         console.log(income[len - 1]);
-        console.log("$ Status: " + user.current_expense + "/" + user.current_income);
+        //The status of the current balance is offest by the surplus.
+        console.log("$ Status: " + user.current_expense + "/" + (user.current_income-user.surplus));
         }
         return 0;
 }
 
 // Function used for the expenses button
-function expendituresButton(num, type, user, date, week) {
-   expenses.push({
+function expendituresButton(num, type, user) {
+        var tempDate = checkDate();
+        const date = tempDate.split('/');
+        expenses.push({
           //Converts the string input into an interger.
-          value: num,
+          expense: num,
           cat: type,
-          day: date,
-          week: week,
+          day: date[0],
+          week: date[1],
         })
         user.current_expense+=num;
         len = expenses.length;
         console.log(expenses[len - 1]);
-        console.log("$ Status: " + user.current_expense + "/" + user.current_income);
+        console.log("$ Status: " + user.current_expense + "/" + (user.current_income-user.surplus));
 }
 
 // going to be used for saving the user data
@@ -137,29 +143,33 @@ function frequentExpense () {
 }
 
 //Function to output the weekly summary and refresh data.
-function weekSummary(user, date, week) {
+function weekSummary(user) {
+    var tempDate = checkDate();
+    const date = tempDate.split('/');
     //Add in function to save user data here.
     saveButton();
     console.log("Summary;");
     console.log("Total expenses last week: " + user.current_expense);
     console.log("Total income last week: " + user.current_income);
-    console.log("Surplus: " + (user.current_income-user.current_expense));
+    console.log("Surplus: " + (user.current_income-user.current_expense) + "/" + user.surplus);
     console.log('');
-    frequentExpense(user, week);
+    frequentExpense(user, date[1]);
     user.current_expense = 0;
     user.current_income = 0;
     day = 1;
-    weeklyCharges(user, day, week);
+    weeklyCharges(user);
 }
 
 //Fuction to be called on day 1 to add weekly expenses
-function weeklyCharges(user, date, week) {
-    if(date == 1) {
+function weeklyCharges(user) {
+    var tempDate = checkDate();
+    const date = tempDate.split('/');
+    if(date[0] == 1) {
         incomeButton(user.weekly_income, "Weekly Income", user, date, week);
         expendituresButton(user.weekly_expense, "Rent", user, date, week);
         return 1;
     }
-    else if (date == 8) {
+    else if (date[0] == 8) {
         weekSummary(user, date, week);
     }
     else {
@@ -167,8 +177,44 @@ function weeklyCharges(user, date, week) {
     }
 }
 
-
-
+//Function to check the date. In future will be implemented correctly. Is more of a placeholder for the time being.
+function checkDate() {
+  //Currently creates a string in the format "day/week" b/c in the future when we read in the 
+  //actual date we will get a string like "01/12/2024".
+  //The string will get separated in the functions that call checkDate()
+  var readDate = day + "/" + week;
+  return readDate;
+}
+//Function to output the transaction history.
+/*NOTE: We will probably need to make a sorting algorithm at some point, so that the 
+data gets output in a sequential manner. Otherwise the information could be in the wrong order.
+but thats a future problem. */
+function transactionHistory() {
+  console.log("                Transaction history:");
+  console.log("                    ||EXPENSES||");
+  for(i=0; i< expenses.length; i++) {
+    console.log(expenses[i]);
+  }
+    console.log("                    ||INCOME||");
+  for(i=0; i< income.length; i++) {
+    console.log(income[i]);
+  }
+  return 0;
+}
+//Function to set the desired weekly surplus of the user. Would work with a button in the HTML file eventually.
+function setSurplus(user, type, amount) {
+  if (type == "fixed") {
+    user.surplus = amount;
+  }
+  /* Currently setting the surplus as a percentage of the weekly income does not adjust
+  when more income is added. To fix this would requre some more variables and would futher complicate
+  the objects so I decided not fix the issue for now. */
+  else if (type == "percent") {
+    user.surplus = (amount/100) * user.weekly_income;
+  }
+  console.log("Desired weekly surplus is : " + user.surplus);
+  return 0;
+}
 //Create new user.
 test_user = new user();
 test_user.first_name = "Richard";
@@ -193,42 +239,35 @@ var week = 1;
 
 //Day 1
 console.log("Day 1:");
+setSurplus(current_user, "percent", 10);
 //Calls the weekly charges into the account.
-weeklyCharges(current_user, day, week);
-
+weeklyCharges(current_user);
 //Quick Snack
-expendituresButton(5.75, "Starbucks", current_user, day, week);
-
-expendituresButton(45.00, "Gas", current_user, day, week);
-
+expendituresButton(5.75, "Starbucks", current_user);
 //Money from friend
-incomeButton(5, "Friend", current_user, day, week);
+incomeButton(5, "Friend", current_user);
 
 console.log("");
 //Day 2
 console.log("Day 2:");
 day+= 1;
-weeklyCharges(current_user, day, week);
+weeklyCharges(current_user);
 //Groceries
-expendituresButton(100, "Walmart", current_user, day, week);
-expendituresButton(100, "Walmart", current_user, day, week);
-
-expendituresButton(100, "Walmart", current_user, day, week);
-
+expendituresButton(100, "Walmart", current_user);
 console.log('');
 
 //Day 3
 console.log("Day 3");
 day +=1;
-weeklyCharges(current_user, day, week);
+weeklyCharges(current_user);
 //nothing happpens
 console.log('');
 
 //Day 4
 console.log("Day 4");
 day +=1;
-weeklyCharges(current_user, day, week);
-expendituresButton(23, "Walmart", current_user, day, week);
+weeklyCharges(current_user);
+
 
 //nothing happpens
 console.log('');
@@ -236,27 +275,25 @@ console.log('');
 //Day 5
 console.log("Day 5");
 day +=1;
-weeklyCharges(current_user, day, week);
+weeklyCharges(current_user);
 //Buy gas
-expendituresButton(45.00, "Gas", current_user, day, week);
+expendituresButton(45.00, "Gas", current_user);
 console.log('');
 
 //Day 6
 console.log("Day 6");
 day +=1;
-weeklyCharges(current_user, day, week);
+weeklyCharges(current_user);
 //Pick up shift at work.
-incomeButton(50.00, "Extra Shift", current_user, day, week);
+incomeButton(50.00, "Extra Shift", current_user);
 
 console.log('');
 
 //Day 7
 console.log("Day 7");
 day +=1;
-weeklyCharges(current_user, day, week);
-expendituresButton(22.00, "Gas", current_user, day, week);
-expendituresButton(22.00, "Gas", current_user, day, week);
-expendituresButton(5.75, "Starbucks", current_user, day, week);
+weeklyCharges(current_user);
+expendituresButton(5.75, "Starbucks", current_user);
 
 
 //nothing happens
@@ -266,14 +303,18 @@ console.log('');
 console.log("Day 1");
 day += 1;
 week = 2;
-weeklyCharges(current_user, day, week);
+weeklyCharges(current_user);
 console.log('');
 
+//Checking purcahse history
+transactionHistory();
 
 //Todo
 //Roll over data into next week. Use the "week" parameter from each object to tell what week it is from.
 //create check date function?
 //Add comments to reoccuring expenses
+
+
 
 //TEST CASES:
 
@@ -282,7 +323,7 @@ console.log('');
 
 //To ben: first create the checks within the functions, then confirm that the checks work by using the test cases.
 //Add edge cases for date and week.
-console.log("Testing:");
+/*console.log("Testing:");
 console.log("");
 test = new user();
 test.first_name = "Test";
@@ -355,5 +396,13 @@ testIncomeButton();
 function testExpendituresButton(num, type, user, date, week) {
     
 }
+
+
+*/
+
+
+
+
+
 
 
