9c4d8fc4ae37574c9986ae15af873a738a2a6f7e
Ciddous <123509482+Ciddous@users.noreply.github.com>
2024-11-24 15:38:43 -0600

    Changed back to using text files

--- functions.js
+++ functions.js
@@ -7,111 +7,170 @@ function changeColor() {
     document.getElementById("my-div").style.backgroundColor = "#FDF305";
 
 }
-
-// User constructor
+//This is the user object constructor.
 function user(first_name, last_name, weekly_income, weekly_expense, current_income, current_expense) {
-    this.first_name = first_name;
-    this.last_name = last_name;
-    this.weekly_income = weekly_income;
-    this.weekly_expense = weekly_expense;
-    this.current_income = current_income;
-    this.current_expense = current_expense;
+    this.first_name = '';
+    this.last_name = '';
+    //These two are the projected values for each week.
+    this.weekly_income = 0;
+    this.weekly_expense = 0;
+    //These two are the real world values for each week.
+    this.current_income = 0;
+    this.current_expense = 0;
 }
 
-// Function to create a user
+currentUser = new user();
+
+// This function will create new users into the database
+// It take all the data inputted when creating a new user and add into the databse using the proper format
+// So that way we log in the user we make sure the data is called back in properly and not out of order
 function createUser(username, password, firstName, lastName, weeklyIncome, weeklyExpense, currentIncome, currentExpense) {
-    db.get(`SELECT username FROM users WHERE username = ?`, [username], (err, row) => {
-        if (err) {
-            console.error("Database error:", err.message);
+    const filePath = 'users.txt';
+
+    let fileContent;
+    try {
+        fileContent = fs.readFileSync(filePath, 'utf8');
+    } catch (err) {
+        if (err.code === 'ENOENT') {
+            console.log("File not found")
+            fileContent = "";
+        }
+        else {
+            console.error("Error reading file: ", err);
             return;
         }
+    }
 
-        if (row) {
+    const users = fileContent.split('\n').filter(Boolean);
+    for (let user of users) {
+        const userFields = user.split(',');
+        if (userFields[0] === username) {
             console.log("Error: Username already exists");
             return;
         }
+    }
+    
+    const userLine = `${username},${password},${firstName},${lastName},${weeklyIncome},${weeklyExpense},${currentIncome},${currentExpense}\n`;
 
-        const sql = `INSERT INTO users (username, password, first_name, last_name, weekly_income, weekly_expense, current_income, current_expense)
-                     VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
+    fs.appendFile('users.txt', userLine, (err) => {
+        if (err) {
+            console.error("error");
+        } 
+        else {
+            console.log("success");
+        }
+    });
+}
+// This function will take the inputted username and password given by the user and then see if it is within the database
+// If found in the database it will copy all the data to the user
+// If not found it will say user not found
+function login(username, password){
+    // First it reads in the file "users.txt" that contains the data base
+    // If fails it will return 
+    fs.readFile('users.txt', 'utf8', (err, data) => {
+        if (err) {
+            console.error('Error reading the file:', err);
+            return;
+        }
 
-        db.run(sql, [username, password, firstName, lastName, weeklyIncome, weeklyExpense, currentIncome, currentExpense], function (err) {
-            if (err) {
-                console.error("Error inserting user:", err.message);
-            } else {
-                console.log("User successfully added with ID:", this.lastID);
+        // creates a const to be used for data splitting
+        const lines = data.split('\n');
+
+        // This will take all the information on a given line and split the data up by the char ','
+        // If the current line it is contains the correct username and password it will read in this data
+        // If that line doesn't contain it, then it will move to the next one
+        // If user can't be found it will tell the system who then tell the user
+        for (const line of lines) {
+
+            const [storedUsername, storedPassword, storedFirstName, storedLastName, storedWeeklyIncome, storedWeeklyExpense, storedCurrentIncome, storedCurrentExpense] = line.split(',');
+
+            if (storedUsername === username && storedPassword === password) {
+                currentUser.first_name = storedFirstName;
+                currentUser.last_name = storedLastName;
+                currentUser.weekly_income = storedWeeklyIncome;
+                currentUser.weekly_expense = storedWeeklyExpense;
+                currentUser.current_income = storedCurrentIncome;
+                currentUser.current_expense = storedCurrentExpense;
+                return;
             }
-        });
+        }
+
+        console.log('User not Found');
     });
 }
 
-// Function to get all users
-function getAllUsers(callback) {
-    db.all("SELECT * FROM users", (err, rows) => {
-        if (err) {
-            return callback(err);
-        }
-        callback(null, rows);
-    });
-}
-
-// Login function
-function login(username, password) {
-    const query = `SELECT first_name, last_name, weekly_income, weekly_expense, current_income, current_expense 
-                   FROM users 
-                   WHERE username = ? AND password = ?`;
-
-    db.get(query, [username, password], (err, row) => {
-        if (err) {
-            console.error('Database error:', err.message);
-            return;
-        }
-
-        if (row) {
-            console.log('Login successful:', row);
-        } else {
-            console.log('User not found');
-        }
-    });
-}
-
-// Function to save user updates
 function saveUser(username, weeklyIncome, weeklyExpense, currentIncome, currentExpense) {
-    const query = `UPDATE users 
-                   SET weekly_income = ?, weekly_expense = ?, current_income = ?, current_expense = ?
-                   WHERE username = ?`;
+    const filePath = 'users.txt'
 
-    db.run(query, [weeklyIncome, weeklyExpense, currentIncome, currentExpense, username], function (err) {
-        if (err) {
-            console.error('Database error:', err.message);
-            return;
+    let fileContent;
+    try {
+        fileContent = fs.readFileSync(filePath, 'utf8');
+    } catch (err) {
+        console.error("Error reading file: ", err);
+        return;
+    }
+
+    const users = fileContent.split('\n').filter(Boolean);
+
+    const updatedUsers = users.map(user => {
+        const userFields = user.split(',');
+
+        if (userFields[0] === username) {
+            console.log(`Updating user: ${username}`);
+
+            userFields[4] = weeklyIncome;
+            userFields[5] = weeklyExpense;
+            userFields[6] = currentIncome;
+            userFields[7] = currentExpense;
         }
+        return userFields.join(',');
+    });
 
-        if (this.changes > 0) {
-            console.log(`Success: User ${username} updated!`);
-        } else {
-            console.log(`User ${username} not found.`);
+    const newFileContent = updatedUsers.join('\n');
+
+    fs.writeFile(filePath, newFileContent, (err) => {
+        if (err) {
+            console.error("Error writing to file:", err);
+        }
+        else {
+            console.log("Success: User updated!");
         }
     });
 }
 
-// Final step to close the database connection
-process.on('exit', () => {
-    db.close((err) => {
-        if (err) {
-            console.error('Error closing database:', err.message);
-        } else {
-            console.log('Database connection closed.');
-        }
-    });
-});
+// This function will first call the saveUser function to make sure that all the user data is saved into the system
+// Once saved into the system it will result all the currentUser values to zero until another user logins
+function logout() {
+    saveUser();
+    currentUser.first_name = 0;
+    currentUser.last_name = 0;
+    currentUser.weekly_income = 0;
+    currentUser.weekly_expense = 0;
+    currentUser.current_income = 0;
+    currentUser.current_expense = 0;
+}
 
-let expenses = [];
-let income = [];
+//Function to get username
+function getName(user) {
+    console.log(user.first_name + " " + user.last_name);
+}
+// This function updates the current account by adding an income
+function addIncome(trans) {
+    current_user.current_income += trans;
+}
+// This function updates the current account by adding an expense
+function addExpense(trans) {
+    current_user.current_income -= trans;
+}
 
+// Array used to keep track of the expenses inputted by the user
+var expenses = [];
+// Array used to keep track of the income inputted by the user
+var income = [];
 // Function used for the income button
 function incomeButton() {
     // creates a temp variable, that will store the user input into it
-    var temp = document.getElementById('left-income');
+    var temp = document.getElementById('income');
 
     // This if statement will test to see if the user input isn't empty and actually holds data
     // If true it will push he data to the income array
@@ -124,19 +183,12 @@ function incomeButton() {
     else {
         alert("Value Field is Empty, Please Input!");
     }
-
-    document.getElementById('left-income').value = '';
-    document.getElementById('income-label').textContent = 'Income:';
-}
-
-function addIncome(amont) {
-    console.log("Income added:", amount);
 }
 
 // Function used for the expenses button
 function expendituresButton() {
     // creates a temp variable, that will store the user input into it
-    var temp = document.getElementById('left-expenses');
+    var temp = document.getElementById('expense');
 
     // This if statement will test to see if the user input isn't empty and actually holds data
     // If true it will push he data to the expenses array
@@ -149,11 +201,20 @@ function expendituresButton() {
     else {
         alert("Value Field is Empty, Please Input!");
     }
-
-    document.getElementById('left-expenses').value = '';
-    document.getElementById('expenses-label').textContent = 'Expenses:';
 }
 
-function addExpense(amont) {
-    console.log("Expense added:", amount);
+// going to be used for saving the user data
+function saveButton() {
+
 }
+
+// used for taking all the data and calculate their budget
+function budgetCalc() {
+
+}
+
+//This function updates the user's name when the page loads.
+//This is how you have to wrtie anything that you want to happen right away.
+//window.onload = function() {
+//   document.getElementById('username_html').innerHTML = test_user.first_name + " " + test_user.last_name;
+//};
